3d11fafba44926b1a621be6c8df5e404d9752ba6
shubhbrth <shbhmbarthwal94@gmail.com>
2018-06-13 21:44:46 +0530

    Message Box Added

---
+++ check.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 20:34:44 2018
+
+@author: shubham
+"""
+
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QSize    
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(300, 200))    
+        self.setWindowTitle("PyQt messagebox example - pythonprogramminglanguage.com") 
+
+        pybutton = QPushButton('Show messagebox', self)
+        pybutton.clicked.connect(self.clickMethod)
+        pybutton.resize(200,64)
+        pybutton.move(50, 50)        
+
+    def clickMethod(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("This is a message box")
+        msg.setInformativeText("This is additional information")
+        msg.setWindowTitle("MessageBox demo")
+        msg.setDetailedText("The details are as follows:")
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        retval = msg.exec_()
+        print ("value of pressed message box button:", retval)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
